1ebafa95598bf8affed6621657eb81999e88833e
AndrewIndeche <indecheandrew@gmail.com>
2022-06-02 10:48:45 +0300

    setup-react and update package json for engine

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+var appNode = document.getElementById("app");
+ReactDOM.render(<App name="Jane" />, appNode);
