45dac6a5da69316c08c9a4651bf2bff3e6789403
AndrewIndeche <indecheandrew@gmail.com>
2022-06-04 13:13:38 +0300

    change position of homepage component

--- src/homepage/Homepage.js
+++ src/homepage/Homepage.js
@@ -1,14 +1,27 @@
 import React from "react";
+import Rules from "./Rules.js";
+import Results from "./Results.js";
+import '/node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "../style.css";
+
 
 class Homepage extends React.Component {
+    
     render() {
-        const { name } = this.props;
-        return ( <
-            h1 >
-            Hello { name } <
-            /h1 >
+        //const { name } = this.props;
+        return ( 
+            <>
+            <h1 class="card-header" style={{color:"red"}}>
+            Guess the Number
+            </h1 >
+            <div className = "card-body">
+            <Rules/>
+            <Results/>
+            </div>         
+            </>
         );
     }
 }
 
 export default Homepage;
+
