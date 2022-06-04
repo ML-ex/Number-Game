d5bb0bc5e89aff12b3c3cdae5a5f15b49c1b098b
AndrewIndeche <indecheandrew@gmail.com>
2022-06-04 13:06:07 +0300

    add logic to generate random number,add logic to get user input,add logic to turn and return success or error pages,add logic to limit game to 3 turns

--- src/homepage/Game.js
+++ src/homepage/Game.js
@@ -1,30 +1,101 @@
-import React from "react";
+import React, { Component } from 'react';
 
-class Game extends React.Component {
-    render() {
+class Game extends Component {
 
-        const { name } = this.props;
-        return ( 
-            <>
-            <h2>Play the Game here</h2>
-             <form>
-                <label>
-                <input type="text" name="name" />
-                Guess:
-                </label>
-                <label>
-                    <input type="text" name="name" />
-                </label>
-                <button
-                    type='button'>
-                        PLAY AGAIN
-                        </button>
-                        <button
-                    type='button'>
-                        EXIT
-                        </button>
-            </form>
-            </>
+    constructor(props){
+        super(props);
+        this.state = {
+            randomNumber: Math.floor(Math.random() *100) + 1 + "",
+            guesses: "",
+            lastResult: "",
+            lowOrHi: "",
+            guessCount: 1,
+            classLastResult: "",
+            classButtonNewGame: 'd-none'
+        };
+
+        this.checkGuess = this.checkGuess.bind(this);
+    }
+
+    componentDidMount(){
+        this.guessNumber.focus();
+    }
+
+    componentDidUpdate(){
+        if(this.state.classButtonNewGame === "btn btn-primary m-2"){
+            this.startNewGame.focus();
+        }
+    }
+
+    checkGuess(event){
+        event.preventDefault();
+        let guessValue = event.target.guessNumber.value;
+        let randomValue = this.state.randomNumber;
+        event.target.guessNumber.value = "";
+
+        if(guessValue !== "") {
+            this.setState((prevState) => ({
+                guesses: prevState.guesses === "" ? `Previous guesses: ${guessValue}` : `${prevState.guesses}, ${guessValue}`,
+                guessCount: prevState.guessCount + 1
+            }));
+            if (guessValue === randomValue) {
+                this.setState({
+                    lastResult: "Good Job,You got it correct!",
+                    lowOrHi: "",
+                    classLastResult: "m-2 p-1 bg-success",
+                    classButtonNewGame: "btn btn-primary m-2"
+                });
+
+                this.submitGuess.setAttribute("disabled", "disabled");
+                this.guessNumber.setAttribute("disabled", "disabled");
+
+            } else if (this.state.guessCount === 3) {
+                this.setState({
+                    lastResult: "GAME OVER!",
+                    lowOrHi: "",
+                    classLastResult: "m-2 p-1 bg-danger",
+                    classButtonNewGame: "btn btn-primary m-2"
+                });
+
+                this.submitGuess.setAttribute("disabled", "disabled");
+                this.guessNumber.setAttribute("disabled", "disabled");
+
+
+            } else if (guessValue > randomValue) {
+                this.setState({
+                    lastResult: "Wrong!",
+                    lowOrHi: "Last guess was too high!",
+                    classLastResult: "m-2 p-1 bg-danger"
+                });
+            } else if (guessValue < randomValue) {
+                this.setState({
+                    lastResult: "Wrong!",
+                    lowOrHi: "Last guess was too low!",
+                    classLastResult: "m-2 p-1 bg-danger"
+                })
+            } 
+
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <form className="form-inline" onSubmit={this.checkGuess}>
+                        <h3 className="m-2">Give It A Try Below:</h3>
+                        <input name="guessNumber" type="number" min="1" max="100" ref={(input) => {this.guessNumber = input;}} className="form-control m-2"/>
+                        <button type="submit" ref={(button) => {this.submitGuess = button;}} className="btn btn-primary m-2">GUESS!</button>
+                </form>
+                <div>
+                    <p className="m-2">{this.state.guesses}</p>
+                    <p className={this.state.classLastResult}>{this.state.lastResult}</p>
+                    <p className="m-2">{this.state.lowOrHi}</p>
+                    
+                    
+                    <button ref={(button) => {this.startNewGame = button;}} className={this.state.classButtonNewGame} onClick={this.props.newGame}>Play Again</button>
+                    <button type='button'>EXIT GAME </button>
+                </div>
+            </div>
         );
     }
 }
