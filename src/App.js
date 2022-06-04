7645daa73a04fd9c7489a64d27a59efb4b9446c3
rosekairu <rosekairu@gmail.com>
2022-06-04 12:57:46 +0300

    resolved missing semicolon in App js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { hot } from 'react-hot-loader/root';
-import Homepage from './homepage/Homepage.js'
+import Homepage from './homepage/Homepage.js';
 
 class App extends React.Component {
     render() {
